cdda6e3dfb4fd71e21776c47a49d96c9d192fdae
Zhekanevi <zekanevip23@gmail.com>
2023-12-08 19:59:09 +0200

    full

--- test_ruffier-main/timer.py
+++ test_ruffier-main/timer.py
@@ -5,26 +5,28 @@ from kivy.uix.label import BooleanProperty
 
 
 class Timer(Label):
-    BooleanProperty(False)
+    done = BooleanProperty(False)
+
     def __init__(self, **kwargs):
         self.done(False)
-        self.total = total
-        self.current = 0
-        my_text = "Пройшло секунд: " + str(current)
-        super().__init__(text=my_text)
+        self.total = total # кількість секунд в таймері
+        self.current = 0 # лічильник секунд
+        my_text = "Пройшло секунд: " + str(current) # текст віджету таймер
+        super().__init__(text=my_text) # виклик конструктора супер класу
 
 
-    def start(self):
-        Clock.schedule_interval(self.change, 1)
+    def start(self): # метод запуску таймеру
+        Clock.schedule_interval(self.change, 1) # запуск методу self.change кожну секунду
 
 
-    def change(self, dt):
-        self.current += 1
-        my_text = "Пройшло секунд: " + str(current)
+    def change(self, dt): # функція обробник 
+        self.current +=  # збільшуємо секунди
+        my_text = "Пройшло секунд: " + str(current) #  міняємо текст
         super().__init__(text=my_text)
         self.done(True)
-        if self.current == self.total:
-            return False
+        if self.current == self.total: # умова зупинки таймеру
+            self.done = True # міняємо властивість
+            return False # зупиняємо таймер
 
 
 
